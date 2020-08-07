11180d037ced8c3d270129c2450adc9c8379f46c
Erik1404 <kietnguyen1998@gmail.com>
2020-08-07 13:15:31 +0700

    con 3

--- script for setting.js
+++ script for setting.js
@@ -1,9 +1,27 @@
 function hien1() {
     document.getElementById("hide1").style.display = "block";
     document.getElementById("hide2").style.display = "none";
+    document.getElementById("hide8").style.display = "none";
+    document.getElementById("hide6").style.display = "none";
 }
 
 function hien2() {
     document.getElementById("hide2").style.display = "block";
     document.getElementById("hide1").style.display = "none";
+    document.getElementById("hide8").style.display = "none";
+    document.getElementById("hide6").style.display = "none";
+}
+
+function hien8() {
+    document.getElementById("hide2").style.display = "none";
+    document.getElementById("hide1").style.display = "none";
+    document.getElementById("hide8").style.display = "block";
+    document.getElementById("hide6").style.display = "none";
+}
+
+function hien6() {
+    document.getElementById("hide2").style.display = "none";
+    document.getElementById("hide1").style.display = "none";
+    document.getElementById("hide8").style.display = "none";
+    document.getElementById("hide6").style.display = "block";
 }
