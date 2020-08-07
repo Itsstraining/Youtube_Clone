f76868835c56f541ff3530346d93ec4dfa707131
Erik1404 <kietnguyen1998@gmail.com>
2020-08-07 13:49:46 +0700

    con 2

--- script for setting.js
+++ script for setting.js
@@ -3,6 +3,8 @@ function hien1() {
     document.getElementById("hide2").style.display = "none";
     document.getElementById("hide8").style.display = "none";
     document.getElementById("hide6").style.display = "none";
+    document.getElementById("hide3").style.display = "none";
+    document.getElementById("hide7").style.display = "none";
 }
 
 function hien2() {
@@ -10,6 +12,8 @@ function hien2() {
     document.getElementById("hide1").style.display = "none";
     document.getElementById("hide8").style.display = "none";
     document.getElementById("hide6").style.display = "none";
+    document.getElementById("hide7").style.display = "none";
+    document.getElementById("hide3").style.display = "none";
 }
 
 function hien8() {
@@ -17,6 +21,8 @@ function hien8() {
     document.getElementById("hide1").style.display = "none";
     document.getElementById("hide8").style.display = "block";
     document.getElementById("hide6").style.display = "none";
+    document.getElementById("hide7").style.display = "none";
+    document.getElementById("hide3").style.display = "none";
 }
 
 function hien6() {
@@ -24,4 +30,24 @@ function hien6() {
     document.getElementById("hide1").style.display = "none";
     document.getElementById("hide8").style.display = "none";
     document.getElementById("hide6").style.display = "block";
+    document.getElementById("hide7").style.display = "none";
+    document.getElementById("hide3").style.display = "none";
+}
+
+function hien7() {
+    document.getElementById("hide2").style.display = "none";
+    document.getElementById("hide1").style.display = "none";
+    document.getElementById("hide8").style.display = "none";
+    document.getElementById("hide6").style.display = "none";
+    document.getElementById("hide7").style.display = "block";
+    document.getElementById("hide3").style.display = "none";
+}
+
+function hien3() {
+    document.getElementById("hide2").style.display = "none";
+    document.getElementById("hide1").style.display = "none";
+    document.getElementById("hide8").style.display = "none";
+    document.getElementById("hide6").style.display = "none";
+    document.getElementById("hide7").style.display = "none";
+    document.getElementById("hide3").style.display = "block";
 }
